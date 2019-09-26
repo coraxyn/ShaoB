f0526d6c48516e3699e0fbd53acc859ba41b9eb0
Paul Davidson <47060199+coraxyn@users.noreply.github.com>
2019-09-26 09:41:05 -0400

    Update sOPS.pas

--- sOPS.pas
+++ sOPS.pas
@@ -1,1 +1,20 @@
-{  sOPS.pas  Copyright (c) 2019 Paul Davidson. All rights reserved.  Lists OPS for current channel  via /msg chanserv access #channel list}unit sOPS;  {$MODE OBJFPC}  {$H+}interface  uses
+{ 
+  sOPS.pas
+  Copyright (c) 2019 Paul Davidson. All rights reserved.
+
+  Lists OPS for current channel
+  via /msg chanserv access #channel list
+}
+
+
+unit sOPS;
+
+
+  {$MODE OBJFPC}
+  {$H+}
+
+
+interface
+
+
+  uses
