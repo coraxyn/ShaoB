8d6fd2cc28350e4a0f618943adbd7143cb6dec06
Paul Davidson <47060199+coraxyn@users.noreply.github.com>
2019-09-26 09:32:16 -0400

    Now in main

--- Source/Quake.pas
+++
@@ -1,1 +0,0 @@
-program Quake;  uses    cThreads,    cMem,    sCurl,    sQuake;      var    fQuake : tQuake;          begin    fCurl := TCurl.Create;    writeln( 'Quake' );    fQuake := tQuake.Create;    fQuake.Start;    readln;    fCurl.Free;    fQuake.Free;  end.
